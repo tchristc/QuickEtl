1b24879391135e9334eb366523471c701be13461
WiDpiTom <thomas.christensen@dpi.wi.gov>
2017-04-19 09:01:09 -0500

    bulk copy parallel

--- QuickEtl.Test.ConsoleApp/Program.cs
+++ QuickEtl.Test.ConsoleApp/Program.cs
@@ -10,8 +10,15 @@ namespace QuickEtl.Test.ConsoleApp
     {
         static void Main(string[] args)
         {
-            var class1 = new Class1();
-            class1.Start();
+            //var class1 = new Class1();
+            //class1.Start();
+            //"data source=SQLDWV02;integrated security=True;MultipleActiveResultSets=True;application name=Ods_Validation"
+            var copy = new BulkCopyParallel();
+            copy.Copy("data source=SQLSWV06;initial catalog=EdFi_Api_2017;integrated security=True;application name=SqlBulkCopy", 
+                "edfi.Student", 
+                "data source=(local);initial catalog=Toms_Ods;integrated security=True;application name=SqlBulkCopy", 
+                "dbo.Student");
+
         }
     }
 }
--- QuickEtl/BulkCopy.cs
+++ QuickEtl/BulkCopy.cs
@@ -1,8 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Data.SqlClient;
-using System.Linq;
-using System.Text;
+using System.Diagnostics;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace QuickEtl
@@ -16,23 +15,129 @@ namespace QuickEtl
     {
         public void Copy(string sourceConnectionString, string sourceTable, string targetConnectionString, string targetTable)
         {
+            Stopwatch watch = new Stopwatch();
+            watch.Start();
+
             using (var testConnection = new SqlConnection(sourceConnectionString))
             {
                 testConnection.Open();
                 var command = new SqlCommand($"select * from {sourceTable};", testConnection);
                 using (var reader = command.ExecuteReader())
                 {
-                    using (var destinationConnection = new SqlConnection(targetConnectionString))
+                    //using (var destinationConnection = new SqlConnection(targetConnectionString))
+                    //{
+                    
+                    using (var bc = new SqlBulkCopy(targetConnectionString, SqlBulkCopyOptions.KeepIdentity | SqlBulkCopyOptions.UseInternalTransaction | SqlBulkCopyOptions.TableLock))
                     {
-                        using (var bc = new SqlBulkCopy(destinationConnection))
-                        {
-                            destinationConnection.Open();
-                            bc.DestinationTableName = targetTable;
-                            bc.WriteToServer(reader);
-                        }
+
+                        //destinationConnection.Open();
+                        bc.BatchSize = 0;
+                        bc.BulkCopyTimeout = 1800;
+                        bc.DestinationTableName = targetTable;
+                        bc.WriteToServer(reader);
                     }
+                    //}
                 }
             }
+
+            watch.Stop();
+            Console.WriteLine("Elapsed: " + watch.ElapsedMilliseconds);
+            Console.ReadLine();
+
+            // 1. 105420ms ~1.75min (100K batchsize)
+            // 2. 84861 (default batch size)
+
+            //WITH OrderedOrders AS
+            //(
+            //    SELECT SalesOrderID, OrderDate,
+            //    ROW_NUMBER() OVER (ORDER BY OrderDate) AS 'RowNumber'
+            //    FROM Sales.SalesOrderHeader 
+            //) 
+            //SELECT * 
+            //FROM OrderedOrders 
+            //WHERE RowNumber BETWEEN 51 AND 60;
+
+//            SELECT*
+//FROM Sales.SalesOrderHeader
+//ORDER BY OrderDate
+//OFFSET(@Skip) ROWS FETCH NEXT(@Take) ROWS ONLY
+
+        }
+    }
+
+    public class BulkCopyParallel : IBulkCopy
+    {
+        public const int BATCH_SIZE = 500;
+        public static object LOCKER = new object();
+        
+        public void Copy(string sourceConnectionString, string sourceTable, string targetConnectionString, string targetTable)
+        {
+
+            using (var testConnection = new SqlConnection(targetConnectionString))
+            {
+                testConnection.Open();
+                var command = new SqlCommand($"truncate table {targetTable}", testConnection);
+                command.ExecuteNonQuery();
+            }
+
+            var studentCount = 0;
+            using (var testConnection = new SqlConnection(sourceConnectionString))
+            {
+                testConnection.Open();
+                var command = new SqlCommand($"select count(*) from {sourceTable};", testConnection);
+                studentCount = (int)command.ExecuteScalar();
+            }
+
+            var watch = new Stopwatch();
+            watch.Start();
+
+            int counter = 0;
+            Parallel.For(0, studentCount/BATCH_SIZE, new ParallelOptions { MaxDegreeOfParallelism = Environment.ProcessorCount }, i =>
+            {
+                int skip;
+                lock (LOCKER)
+                {
+                    skip = counter * BATCH_SIZE;
+                    Interlocked.Increment(ref counter);
+                }
+                var done = false;
+                while (!done)
+                {
+                    try
+                    {
+                        using (var testConnection = new SqlConnection(sourceConnectionString))
+                        {
+                            testConnection.Open();
+                            var command = new SqlCommand($"select * from {sourceTable} " +
+                                                         $"ORDER BY Id " +
+                                                         $"OFFSET({skip}) ROWS FETCH NEXT({BATCH_SIZE}) ROWS ONLY",
+                                testConnection);
+                            using (var reader = command.ExecuteReader())
+                            {
+                                using (
+                                    var bc = new SqlBulkCopy(targetConnectionString,
+                                        SqlBulkCopyOptions.KeepIdentity | SqlBulkCopyOptions.UseInternalTransaction))
+                                {
+                                    bc.BatchSize = 0;
+                                    bc.BulkCopyTimeout = 1800;
+                                    bc.DestinationTableName = targetTable;
+                                    bc.WriteToServer(reader);
+                                }
+                            }
+                        }
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine(e);
+                        continue;
+                    }
+                    done = true;
+                }
+            });
+
+            watch.Stop();
+            Console.WriteLine("Elapsed: " + watch.ElapsedMilliseconds);
+            Console.ReadLine();
         }
     }
 }
